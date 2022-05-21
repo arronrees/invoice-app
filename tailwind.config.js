c23076e583820ac4ad2b727ccb0a1c0f2e698f47
arronrees <64908445+arronrees@users.noreply.github.com>
2022-05-21 21:01:50 +0100

    Initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    colors: {
+      'light-purple': '#9277FF',
+      'mid-purple': '#7C5DFA',
+      'light-blue': '#7E88C3',
+      'mid-blue': '#252945',
+      'dark-blue': '#1E2139',
+      'light-grey': '#DFE3FA',
+      'mid-grey': '#888EB0',
+      black: '#0C0E16',
+      'light-red': '#9277FF',
+      'mid-red': '#EC5757',
+      'light-bg': '#F8F8FB',
+      'dark-bg': '#141625',
+    },
+    fontFamily: {
+      sans: ['League Spartan', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
