0d267a7d1896271f2c4f887bca8dab9b031ddef3
arronrees <64908445+arronrees@users.noreply.github.com>
2022-05-30 22:10:14 +0100

    Edited sidebar desktop

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -4,7 +4,7 @@ import ThemeToggler from './ThemeToggler';
 
 export default function Header() {
   return (
-    <header className='bg-dark-grey flex items-center justify-between h-20 lg:h-screen lg:flex-col'>
+    <header className='bg-dark-grey flex items-center justify-between h-20 lg:h-screen lg:flex-col lg:fixed'>
       <Logo />
       <ThemeToggler />
       <ProfileBtn />
--- components/layout/Wrapper.jsx
+++ components/layout/Wrapper.jsx
@@ -1,7 +1,7 @@
 export default function Wrapper({ children }) {
   return (
     <section className='mx-auto lg:px-8 lg:mt-16 w-full px-4 max-w-5xl'>
-      {children}
+      <div className='lg:pl-24'>{children}</div>
     </section>
   );
 }
